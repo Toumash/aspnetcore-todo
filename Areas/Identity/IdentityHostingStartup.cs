eec404c38aae01098f0b0ce53ab07ac09e763523
Tomasz Dłuski <mr.toumash@gmail.com>
2020-01-11 23:43:42 +0100

    scafolded login page to remove external connectors

---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using aspnetcore_todo.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(aspnetcore_todo.Areas.Identity.IdentityHostingStartup))]
+namespace aspnetcore_todo.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
